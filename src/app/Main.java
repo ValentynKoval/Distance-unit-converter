8dcf019d93c60fd13bd1dd98c14895deaf9d898d
Valentyn_Koval <J.K.play2015@gmail.com>
2025-12-16 21:40:36 +0200

    add conversion of miles to kilometers

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,17 @@
 package app;
 
 public class Main {
+    private static final double CONV_K = 0.621371;
+
     public static void main(String[] args) {
         System.out.println("App for measures converting.");
+        double klm = 90;
+        double mi = 20;
+        double kilometers = convMiToKlm(mi);
+        System.out.println("Kilometers: " + kilometers);
+    }
+
+    private static double convMiToKlm(double mi) {
+        return mi / CONV_K;
     }
 }
